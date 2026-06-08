fadb9d8c6fd6a4b51acb38ec48197e1e127fd07e
Yusuf Bahadur <yusuf.bahadur@redis.com>
2026-06-08 16:44:46 -0400

    Remove signalwave

--- tests/test_signalwave_domain.py
+++
@@ -1,6 +0,0 @@
-from backend.app.core.domain_loader import load_domain
-
-
-def test_signalwave_domain_loads() -> None:
-    domain = load_domain("signalwave")
-    assert domain.manifest.id == "signalwave"
